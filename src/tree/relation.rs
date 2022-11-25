6b57fa1ade8333ff935f1b059e92ee70893982c9
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2022-11-25 15:20:42 +0100

    Made valueType enum compile and small renaming.

--- src/tree/relation.rs
+++ src/tree/relation.rs
@@ -1,8 +1,8 @@
-use super::schal_path::SchaclPath;
+use super::shacl_path::ShaclPath;
 use super::value::Value;
 pub struct Relation{
-    pub remaning_iterms: Option<i32>,
-    pub path: Option<SchaclPath>,
+    pub remaning_items: Option<i32>,
+    pub path: Option<ShaclPath>,
     pub value: Option<Value>,
     pub node: String,
 }
--- src/tree/value.rs
+++ src/tree/value.rs
@@ -1,27 +1,64 @@
+use std::fmt;
+
 pub struct Value {
-    pub value: string,
-    pub value_type: ValueType
+    pub value: String,
+    pub value_type: ValueType,
 }
 
 pub enum ValueType {
-    Integer = "http://www.w3.org/2001/XMLSchema#integer",
-    Decimal = "http://www.w3.org/2001/XMLSchema#decimal",
-    Float = "http://www.w3.org/2001/XMLSchema#float",
-    Double = "http://www.w3.org/2001/XMLSchema#double",
-    String = "http://www.w3.org/2001/XMLSchema#string",
-    Boolean = "http://www.w3.org/2001/XMLSchema#boolean",
-    DateTime = "http://www.w3.org/2001/XMLSchema#dateTime",
+    Integer,
+    Decimal,
+    Float,
+    Double,
+    String,
+    Boolean,
+    DateTime,
 
-    NonPositiveInteger = "http://www.w3.org/2001/XMLSchema#nonPositiveInteger",
-    NegativeInteger = "http://www.w3.org/2001/XMLSchema#negativeInteger",
-    Long = "http://www.w3.org/2001/XMLSchema#long",
-    Int = "http://www.w3.org/2001/XMLSchema#int",
-    Short = "http://www.w3.org/2001/XMLSchema#short",
-    Byte = "http://www.w3.org/2001/XMLSchema#byte",
-    NonNegativeInteger = "http://www.w3.org/2001/XMLSchema#nonNegativeInteger",
-    UnsignedLong = "http://www.w3.org/2001/XMLSchema#nunsignedLong",
-    UnsignedInt = "http://www.w3.org/2001/XMLSchema#unsignedInt",
-    UnsignedShort = "http://www.w3.org/2001/XMLSchema#unsignedShort",
-    UnsignedByte = "http://www.w3.org/2001/XMLSchema#unsignedByte",
-    PositiveInteger = "http://www.w3.org/2001/XMLSchema#positiveInteger",
+    NonPositiveInteger,
+    NegativeInteger,
+    Long,
+    Int,
+    Short,
+    Byte,
+    NonNegativeInteger,
+    UnsignedLong,
+    UnsignedInt,
+    UnsignedShort,
+    UnsignedByte,
+    PositiveInteger,
+}
+
+impl fmt::Display for ValueType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let string_representation: &str = {
+            match self {
+                ValueType::Integer => "http://www.w3.org/2001/XMLSchema#integer",
+                ValueType::Decimal => "http://www.w3.org/2001/XMLSchema#decimal",
+                ValueType::Float => "http://www.w3.org/2001/XMLSchema#float",
+                ValueType::Double => "http://www.w3.org/2001/XMLSchema#double",
+                ValueType::String => "http://www.w3.org/2001/XMLSchema#string",
+                ValueType::Boolean => "http://www.w3.org/2001/XMLSchema#boolean",
+                ValueType::DateTime => "http://www.w3.org/2001/XMLSchema#dateTime",
+
+                ValueType::NonPositiveInteger => {
+                    "http://www.w3.org/2001/XMLSchema#nonPositiveInteger"
+                }
+                ValueType::NegativeInteger => "http://www.w3.org/2001/XMLSchema#negativeInteger",
+                ValueType::Long => "http://www.w3.org/2001/XMLSchema#long",
+                ValueType::Int => "http://www.w3.org/2001/XMLSchema#int",
+                ValueType::Short => "http://www.w3.org/2001/XMLSchema#short",
+                ValueType::Byte => "http://www.w3.org/2001/XMLSchema#byte",
+                ValueType::NonNegativeInteger => {
+                    "http://www.w3.org/2001/XMLSchema#nonNegativeInteger"
+                }
+                ValueType::UnsignedLong => "http://www.w3.org/2001/XMLSchema#nunsignedLong",
+                ValueType::UnsignedInt => "http://www.w3.org/2001/XMLSchema#unsignedInt",
+                ValueType::UnsignedShort => "http://www.w3.org/2001/XMLSchema#unsignedShort",
+                ValueType::UnsignedByte => "http://www.w3.org/2001/XMLSchema#unsignedByte",
+                ValueType::PositiveInteger => "http://www.w3.org/2001/XMLSchema#positiveInteger",
+            }
+        };
+
+        write!(f, "Circle of radius {}", string_representation)
+    }
 }
