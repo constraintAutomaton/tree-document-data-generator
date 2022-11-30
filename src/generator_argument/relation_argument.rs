11e88d593a07245b7fc3fd7ceeddc468f659fa6e
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2022-11-30 08:58:21 +0100

    In argument support for multiple relation by node

--- src/generator_argument/relation_argument.rs
+++ src/generator_argument/relation_argument.rs
@@ -2,20 +2,28 @@ use std::vec::Vec;
 use crate::tree::relation::Relation;
 use super::RangeParameter;
 
-/// The type of TREE relation generator
+/// The type of TREE relation generator.
 pub enum RelationGeneratorArg<T> {
     /// Generate the exact relations dictated by the user.
-    Direct(Vec<Relation>),
+    Direct(Vec<Vec<Relation>>),
     /// Generate the [`Relation`] based on a template
-    /// and make the [`Relation`] value vary following a [`RangeParameter`] 
+    /// and make the [`Relation`] value vary following a [`RangeParameter`]. 
     ValueVariation(TemplateRangeVariationRelation<T>)   
 }
 
 pub struct TemplateRangeVariationRelation<T>{
-    /// template of the [`Relation`]
+    /// template of the [`Relation`].
     pub template: Relation,
-    /// range of the selected property
+    /// range of the selected property.
     pub range: Box<dyn RangeParameter<T>>,
-    /// number of [`Relation`]
-    pub n: u8
+    /// distribution of  the [`Relation`] inside the [Node](`crate::tree:node::Node`).
+    pub distribution_of_relation: DistributionOfRelation,
+}
+
+/// distribution of the [`Relation`] inside the [Node](`crate::tree:node::Node`).
+pub enum DistributionOfRelation{
+    /// directly set the number of relation by node.
+    Direct(Vec<u8>),
+    /// Set a random number of relation using a [`RangeParameter`] with a number of node.
+    Random(Box<dyn RangeParameter<u8>>, u8)
 }
